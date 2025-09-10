dbb27e8c9ba7d3ca9cbd7d182e97dcc6225f3bf5
kiriashi <2244193927@qq.com>
2025-09-10 21:07:06 +0800

    update

--- pkgs/lx-music-desktop/default.nix
+++ pkgs/lx-music-desktop/default.nix
@@ -1,16 +1,18 @@
-# pkgs/lx-music-desktop/default.nix
-{ 
-stdenv, 
-appimageTools, 
-makeDesktopItem,
-sources
+{ stdenv
+, lib
+, appimageTools
+, sources
+, makeDesktopItem
 }:
 
 let
   pname = "lx-music-desktop";
   version = sources.lx-music-desktop.version;
-  
   src = sources.lx-music-desktop.src;
+
+  extracted = appimageTools.extractType2 {
+    inherit pname version src;
+  };
 
   desktopItem = makeDesktopItem {
     name = pname;
@@ -21,13 +23,13 @@ let
     categories = [ "Audio" "Music" ];
   };
 
-in stdenv.mkDerivation rec {
+in stdenv.mkDerivation {
   inherit pname version src;
 
   nativeBuildInputs = [ appimageTools ];
 
   installPhase = ''
-    # 创建应用目录
+    # 创建目录结构
     mkdir -p $out/bin
     mkdir -p $out/share/applications
     mkdir -p $out/share/icons/hicolor/512x512/apps
@@ -39,13 +41,15 @@ in stdenv.mkDerivation rec {
     # 安装桌面文件
     cp ${desktopItem}/share/applications/* $out/share/applications
     
-    # 提取图标
-    ${appimageTools.wrapType2 {
-      name = pname;
-      src = src;
-      extraPkgs = pkgs: [];
-    }}/bin/${pname} --appimage-extract
-    cp squashfs-root/usr/share/icons/hicolor/512x512/apps/lx-music-desktop.png $out/share/icons/hicolor/512x512/apps/${pname}.png
+    # 复制图标
+    cp ${extracted}/usr/share/icons/hicolor/512x512/apps/lx-music-desktop.png $out/share/icons/hicolor/512x512/apps/${pname}.png
   '';
 
+  meta = with lib; {
+    description = "A free and open-source music player";
+    homepage = "https://github.com/lyswhut/lx-music-desktop";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ];
+    platforms = [ "x86_64-linux" ];
+  };
 }
