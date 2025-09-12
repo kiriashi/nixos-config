ed39e96ef102589599882d3bae692a478dfbaaa8
kiriashi <78373893+kiriashi@users.noreply.github.com>
2025-09-12 17:57:23 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -16,10 +16,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # niri-flake = {
-    #   url = "github:sodiboo/niri-flake";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    niri-flake = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
@@ -31,7 +31,7 @@ outputs =
   { 
     chaotic, 
     home-manager, 
-    # niri-flake, 
+    niri-flake, 
     nix-index-database, 
     nixpkgs, 
     self, 
@@ -67,12 +67,12 @@ outputs =
         })
 
         # niri平铺窗口管理器
-        # niri-flake.nixosModules.niri
-        # ({ lib, ... }: {
-        #   niri-flake.cache.enable = true;
-        #   nixpkgs.overlays = [ niri-flake.overlays.niri ];
-        #   services.gnome.gnome-keyring.enable = lib.mkForce false;
-        # })
+        niri-flake.nixosModules.niri
+        ({ lib, ... }: {
+          niri-flake.cache.enable = false;
+          nixpkgs.overlays = [ niri-flake.overlays.niri ];
+          services.gnome.gnome-keyring.enable = lib.mkForce false;
+        })
 
         # Home Manager 配置
         home-manager.nixosModules.home-manager
