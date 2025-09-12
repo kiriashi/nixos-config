976f0a96b5dd375e9002580eba905c47f4cc98bb
kiriashi <78373893+kiriashi@users.noreply.github.com>
2025-09-12 19:49:54 +0800

    Update nix-ld.nix

--- services/system/nix-ld.nix
+++ services/system/nix-ld.nix
@@ -7,6 +7,7 @@
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
+      (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
       ffmpeg
       icu
     ];
