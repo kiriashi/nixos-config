2242fef2c2c66f91f95ff9317202b1fa767fb4f3
kiriashi <78373893+kiriashi@users.noreply.github.com>
2025-09-12 18:05:20 +0800

    Update hardware-configuration.nix

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -14,37 +14,37 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/bfbf52b3-2eaf-4d50-8d6a-ff6ee9de8077";
+    { device = "/dev/disk/by-uuid/e38711a9-37f2-4aff-92b2-9cdb19247665";
       fsType = "btrfs";
       options = [ "subvol=@" "compress=zstd" "noatime" ];
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/bfbf52b3-2eaf-4d50-8d6a-ff6ee9de8077";
+    { device = "/dev/disk/by-uuid/e38711a9-37f2-4aff-92b2-9cdb19247665";
       fsType = "btrfs";
       options = [ "subvol=@home" "compress=zstd" "noatime" ];
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/bfbf52b3-2eaf-4d50-8d6a-ff6ee9de8077";
+    { device = "/dev/disk/by-uuid/e38711a9-37f2-4aff-92b2-9cdb19247665";
       fsType = "btrfs";
       options = [ "subvol=@nix" "compress=zstd" "noatime" ];
     };
 
   fileSystems."/var" =
-    { device = "/dev/disk/by-uuid/bfbf52b3-2eaf-4d50-8d6a-ff6ee9de8077";
+    { device = "/dev/disk/by-uuid/e38711a9-37f2-4aff-92b2-9cdb19247665";
       fsType = "btrfs";
       options = [ "subvol=@var" "compress=zstd" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2B14-0C7A";
+    { device = "/dev/disk/by-uuid/F393-33E9";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/83afde13-19f2-4aac-ba85-9e3d655b2ef9"; }
+    [ { device = "/dev/disk/by-uuid/760c856a-8c28-49dc-8744-2c116554acf1"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
