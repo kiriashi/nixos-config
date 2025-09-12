1994a65edc5e0949534cced8e2117a4c01787d91
kiriashi <78373893+kiriashi@users.noreply.github.com>
2025-09-12 22:35:51 +0800

    更新 niri.nix

--- services/system/niri.nix
+++ services/system/niri.nix
@@ -17,7 +17,7 @@
     waypaper
     socat
 
-    xwayland-satellite
+    xwayland-satellite-unstable
 
     # nemo-with-extensions
     nautilus
@@ -28,7 +28,7 @@
     xwayland.enable = true;
     niri = {
       enable = true;
-      package = pkgs.niri;
+      package = pkgs.niri-unstable;
     };
     gtklock.enable = true;
   };
