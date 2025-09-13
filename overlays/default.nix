c4121a94fb7ea9e4f3fe483186344f780a27eae1
kiriashi <2244193927@qq.com>
2025-09-13 13:35:11 +0800

    update

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,22 @@
 { ... }:
 {
   nixpkgs.overlays = [
+      (self: super: {
+        scx_full = super.scx_full.overrideAttrs (old: {
+          prePatch = (old.prePatch or "") + ''
+            # 修复 bash 路径
+            substituteInPlace meson-scripts/build_bpftool \
+              --replace '/bin/bash' '${super.bash}/bin/bash'
+        
+            # 修复其他硬编码路径
+            find . -type f -exec sed -i \
+              -e 's|/bin/bash|${super.bash}/bin/bash|g' \
+              -e 's|/usr/bin/env|${super.coreutils}/bin/env|g' \
+              {} +
+          '';
+        });
+      })
+
     (final: prev: {
       niri-stable = prev.niri-stable.overrideAttrs (_: {
         doCheck = false;
