91149676fa2e8469425ac19be69c043f9d7dd432
kiriashi <2244193927@qq.com>
2025-09-28 01:03:21 +0800

    update

--- home/programs/shell/starship.nix
+++ home/programs/shell/starship.nix
@@ -13,14 +13,14 @@
       # Username
       username = {
         format = "[$user]($style) ";
-        style_user = "bold blue";
+        style_user = "blue";
         show_always = true;
       };
 
       # Replaced symbol - Nerd Font
       character = {
-        success_symbol = "[❯](bold green)";
-        error_symbol = "[❯](bold red)";
+        success_symbol = "[›](bold green)";
+        error_symbol = "[›](bold red)";
       };
       # Git status
       git_status = {
@@ -30,10 +30,64 @@
         stashed = "";
       };
 
-      # nix-shell
       nix_shell = {
-        symbol = " ";
+        symbol = " ";  # Nix
         heuristic = true;
+      };
+
+      docker_context = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Docker
+        style = "bold blue";
+        only_with_files = false;
+      };
+
+      golang = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Go
+        style = "bold blue";
+      };
+      
+      java = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Java
+        style = "bold yellow";
+      };
+
+      lua = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Lua
+        style = "bold red";
+      }; 
+
+      nodejs = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Node.js
+        style = "bold green";
+      };
+      
+      php = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # PHP
+        style = "bold purple";
+      }; 
+
+      python = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Python
+        style = "bold blue";
+      };
+      
+      ruby = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Ruby
+        style = "bold red";
+      };
+      
+      rust = {
+        format = "[ $symbol ]($style)";
+        symbol = "";  # Rust
+        style = "bold yellow";
       };
     };
   };
