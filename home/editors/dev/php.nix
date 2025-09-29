659ae2bbc608cd8e6b6d888dc934da01bf3a2c86
kiriashi <2244193927@qq.com>
2025-09-29 10:10:23 +0800

    update

--- home/editors/dev/php.nix
+++ home/editors/dev/php.nix
@@ -1,16 +1,59 @@
-{ lib, config, pkgs, ... }:
+{ config, pkgs, ... }:
+lib.mkIf config.optional.dev.php
+{
 
-lib.mkIf config.optional.dev.php {
-  programs.php = {
-    enable = true;
-    package = pkgs.php83;
-  };
-
-  home.packages = with pkgs; [
+  home.packages = with pkgs.php83Packages; [
+    phpEnv
     composer
-    php83Packages.phpstan
-    php83Packages.psalm
-    vscode-extensions.xdebug.php-debug
-    vscode-extensions.bmewburn.vscode-intelephense-client
-  ];
+    # phpmd
+    # phpstan
+    # psalm
+    php-codesniffer
+    ];  
+
+    phpEnv = pkgs.php83.buildEnv {
+      extensions = ({ enabled, all }: enabled ++ (with all; [
+        xdebug
+        ctype
+        bcmath
+        curl
+        dom
+        ds
+        event
+        exif
+        fileinfo
+        filter
+        gd
+        gettext
+        iconv
+        mbstring
+        memcache
+        mysqli
+        openssl
+        opcache
+        pdo
+        pdo_mysql
+        pdo_pgsql
+        pdo_sqlite
+        pgsql
+        posix
+        protobuf
+        readline
+        redis
+        session
+        # simplexml
+        sockets
+        sqlite3
+        # xml
+        # xmlreader
+        # xmlwriter
+        # xsl
+        yaml
+        zip
+        zlib
+    ]));
+    extraConfig = ''
+        xdebug.mode=coverage
+    '';
+  };
 }
--- modules/options/default.nix
+++ modules/options/default.nix
@@ -26,11 +26,11 @@ with lib;
         go = lib.mkEnableOption "go profile";
         rust = lib.mkEnableOption "rust profile";
         node = lib.mkEnableOption "node profile";
-        python = lib.mkEnableOption "go profile";
-        php = lib.mkEnableOption "rust profile";
-        java = lib.mkEnableOption "node profile";
-        lua = lib.mkEnableOption "go profile";
-        ruby = lib.mkEnableOption "rust profile";
+        python = lib.mkEnableOption "python profile";
+        php = lib.mkEnableOption "php profile";
+        java = lib.mkEnableOption "java profile";
+        lua = lib.mkEnableOption "lua profile";
+        ruby = lib.mkEnableOption "ruby profile";
       };
     }; 
     
