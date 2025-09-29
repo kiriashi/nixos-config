a9bafd28aca8c46ba311d325fadf2487d85b6228
kiriashi <2244193927@qq.com>
2025-09-29 11:10:25 +0800

    update

---
+++ pkgs/grub-themes/Arknights_Theresa/default.nix
@@ -0,0 +1,14 @@
+{ stdenv, sources }:
+
+stdenv.mkDerivation rec {
+  pname = "Arknights_Theresa";
+  version = "1.0.0";
+
+  src = sources.Grub-Theme-Arknights_Theresa.src;
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r $src/Arknights_Theresa/* $out/
+  '';
+}
+
